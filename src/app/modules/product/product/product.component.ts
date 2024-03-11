b2b2fd838c9037b7a11591f7be6724fd363d0de0
ZaneMasters <angel.pro98@gmail.com>
2024-03-11 18:48:50 -0500

    listar productos

---
+++ src/app/modules/product/product/product.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ProductService } from '../../shared/services/product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+ 
+  private productService = inject(ProductService);
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  displayedColumns: string[] = ['id', 'name', 'price', 'account', 'category', 'picture','actions'];
+  dataSource = new MatTableDataSource<ProductElement>();
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  getProducts(){
+    this.productService.getProducts().subscribe ((data:any)=>{
+      console.log("respuesta de productos: ",data);
+      this.processProductResponse(data);
+    }, (error:any) => {
+      console.log("error en productos: ",error);
+    }
+    )
+  }
+
+  processProductResponse(resp: any){
+    const dateProduct: ProductElement[] =[];
+    if (resp.metadata[0].code == "00"){
+      let listCproduct = resp.product.products;
+
+      listCproduct.forEach((element: ProductElement) => {
+        element.category = element.category.name;
+        element.picture = 'data:image/jpeg;base64,'+element.picture;
+        dateProduct.push(element);
+      });
+
+      this.dataSource = new MatTableDataSource<ProductElement>(dateProduct);
+      this.dataSource.paginator = this.paginator;
+    }
+
+  }
+
+
+}
+
+export interface ProductElement{
+  id: number;
+  name: string;
+  price: number;
+  account: number;
+  category: any;
+  picture: any;
+}
